0093299f772c2a538b7e0d1a406a95e665afdd57
Khalel <KAbaquin@west-mec.org>
2019-09-16 14:05:59 -0700

    Auto stash before merge of "master" and "origin/master"

---
+++ 02-AJAX/www/script.js
@@ -0,0 +1,89 @@
+/*  Exercise 01_11_01
+
+    Whole Spectrum Energy Solutions
+    Author: 
+    Date:   
+
+    Filename: script.js
+*/
+
+"use strict";
+
+// global variables
+<<<<<<< HEAD:02-AJAX/Exercise-LAMP/www/script.js
+let selectedCity = "Tucson, AZ";
+let weatherReport;
+
+let httpRequest = false;
+
+function getRequestObject() {
+   try {
+      httpRequest = new XMLHttpRequest();
+   } catch (requestError) {
+      document.querySelector("p.error").innerHTML = "Forecast not suported by your browser.";
+      document.querySelector("p.error").style.display = "block";
+=======
+var selectedCity = "Tucson, AZ";
+var weatherReport;
+var httpRequest = false;
+
+function getRequestObject(){
+   alert('to annoy our users...');
+   try {
+      httpRequest = new XMLHttpRequest();
+   } catch (requestError) {
+      console.log(`Our error:  ${requestError}`);
+>>>>>>> Development:02-AJAX/www/script.js
+      return false;
+   }
+   return httpRequest;
+}
+
+function getWeather(evt) {
+   var latitude;
+   var longitude;
+   if (evt.type !== "load") {
+      if (evt.target) {
+         selectedCity = evt.target.innerHTML;
+      } else if (evt.srcElement) {
+         selectedCity = evt.srcElement.innerHTML;
+      }
+   }
+   if (selectedCity === "Tucson, AZ") {
+      latitude = 37.7577;
+      longitude = -122.4376;
+   } else if (selectedCity === "Chicago, IL") {
+      latitude = 41.8337329;
+      longitude = -87.7321555;
+   } else if (selectedCity === "Montreal, QC") {
+      latitude = 45.5601062;
+      longitude = -73.7120832;
+   }
+<<<<<<< HEAD:02-AJAX/Exercise-LAMP/www/script.js
+   if(!httpRequest) httpRequest = getRequestObject();
+   httpRequest.abort();
+   httpRequest.open("get", "solar.php?" + "lat=" + latitude + 
+   "&lng=" + longitude, true );
+=======
+
+   if(!httpRequest) httpRequest = getRequestObject();
+
+   httpRequest.abort();
+   httpRequest.open("get", "solar.php?" + "lat=" + latitude + "&lng" + longitude, true);
+>>>>>>> Development:02-AJAX/www/script.js
+   httpRequest.send(null);
+}
+
+var locations = document.querySelectorAll("section ul li");
+for (var i = 0; i < locations.length; i++) {
+   if (locations[i].addEventListener) {
+      locations[i].addEventListener("click", getWeather, false);
+   } else if (locations[i].attachEvent) {
+      locations[i].attachEvent("onclick", getWeather);
+   }
+}
+if (window.addEventListener) {
+   window.addEventListener("load", getWeather, false);
+} else if (window.attachEvent) {
+   window.attachEvent("onload", getWeather);
+}
